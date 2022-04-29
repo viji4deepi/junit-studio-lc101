8d39d4dd926d1a9def571b78e2a305622a69b3ca
viji4deepi <viji4deepi@gmail.com>
2022-04-28 21:00:56 -0500

    studio work

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,64 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+//    @Test
+//    public void emptyTest() {
+//        assertEquals(true, true);
+//    }
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
 
+    @Test
+    public void onlyBracketsContainsBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][]]"));
+    }
+    @Test
+    public void onlyBracketsContainsThreeBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[]]]]"));
+    }
+    @Test
+    public void onlyBracketsContainsFourBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+    }
+
+    @Test
+    public void onlyBracketsContainsFiveBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void onlyBracketsContainsSixBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+
+
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyBracketsTwoReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyBracketsThreeReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[]]]"));
+    }
+    @Test
+    public void onlyBracketsFourReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+    @Test
+    public void onlyBracketsFiveReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void onlyBracketsSixReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]][]"));
+    }
 }
